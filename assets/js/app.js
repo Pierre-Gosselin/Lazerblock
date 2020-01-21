cbeb6969d306673b9f461bbba83b7cb7965ddd24
pierre gosselin <pierre.gosselin@orange.fr>
2020-01-21 15:13:36 +0100

    template home

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+import '../css/app.css';
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+
+import $ from 'jquery';
