189dc56b8090b94b5c2f31bf3d46c24feb52ce33
shamsia1 <alimishamsya@gmail.com>
2020-01-30 10:44:40 +0100

    add_pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,4 +14,24 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+
+    /**
+     * @Route("/mentionlegal", name="mentionlegal")
+     *
+     */
+    public function mentionlegal()
+    {
+        return $this->render('home/mentionlegal.html.twig');
+    }
+
+
+    /**
+     * @Route("/faq", name="faq")
+     *
+     */
+    public function FAQ()
+    {
+        return $this->render('home/faq.html.twig');
+    }
 }
